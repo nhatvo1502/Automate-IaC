638cc76033b0c8b99306287dd334288a90d960d5
Nhat Vo <NhatVo@rpi.inc>
2024-01-09 13:02:23 -0800

    modify create-resource.yml, destroy-resource.yml. Add main.tf, variables.tf

--- main.tf
+++ main.tf
@@ -4,18 +4,82 @@ provider "aws" {
 
 terraform {
   backend "s3" {
-    bucket = "keypair-1234"
+    bucket = "bucket-4-tfstate-12345"
     key = "terraform.tfstate"
     region = "us-east-1"
     encrypt = true
   }
 }
 
-resource "aws_key_pair" "create_keypair" {
-  key_name = "tien08"
-  public_key = "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCJgKe/Ko/3pXldymY2/bwP70xPJzP6NpKS9iQP450sWFtc5VuG3fqaOvGMzhrS3VUseALddYALiWmgf+cgjdvASeW3eIl54y2253QTme4eb1WOZ2/MvcANBuiJFqItL7SR4xlaAuI48yS8lh7L2OIIpzy8PUV+EcPD9KGKsAicFwIDAQAB"
+# Create a bucket
+resource "aws_s3_bucket" "new_bucket" {
+  bucket = "automate-iac-bucket-123123123"
 }
 
-resource "" "name" {
-  
+# Create a KeyPair
+resource "aws_key_pair" "new_keypair" {
+  key_name = "automate-iac-kp"
+  public_key = "${var.public_key}"
+
+  tags = {
+    Name = "automate-iac"
+  }
+}
+
+# AMI lookup
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  filter {
+    name = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+  owners = ["099720109477"] # Canonical
+}
+
+# Create VPC
+resource "aws_vpc" "automate-iac-vpc" {
+  cidr_block = "172.16.0.0/16"
+  tags = {
+    Name = "automate-iac"
+  }
+}
+
+# Create subnet
+resource "aws_subnet" "automate-iac-subnet" {
+  vpc_id = aws_vpc.automate-iac-vpc.id
+  cidr_block        = "172.16.10.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "automate-iac"
+  }
+}
+
+# Create network interface
+resource "aws_network_interface" "automate-iac-aws_network_interface" {
+  subnet_id = aws_subnet.automate-iac-subnet.id
+
+  tags = {
+    Name = "automate-iac"
+  }
+}
+
+# Create EC2 instance with created key pair
+resource "aws_instance" "new_instance" {
+  ami = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+  key_name = "automate-iac-kp"
+
+  network_interface {
+    network_interface_id = aws_network_interface.automate-iac-aws_network_interface.id
+    device_index = 0
+  }
+
+  tags = {
+    Name = "automate-iac"
+  }
 }
